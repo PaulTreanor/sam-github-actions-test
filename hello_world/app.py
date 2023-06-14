23558239a3bb2d80251966bcef95faa6390520ac
paultreanor <paul.treanor@fourtheorem.com>
2023-06-14 22:56:47 +0100

    Sample change

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "Change stack so it deploys on commit",
             # "location": ip.text.replace("\n", "")
         }),
     }
